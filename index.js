3fdf64e42b20cd61a58df9a87de2cb818ec04847
faheem-dev0 <154550116+faheem-dev0@users.noreply.github.com>
2023-12-27 20:34:42 +0530

    Update index.js

--- index.js
+++ index.js
@@ -23,6 +23,6 @@ function deleted() {
 }
 
 // Event listeners for buttons
-document.getElementById("increment-btn").addEventListener("click", increment)
-document.getElementById("save-btn").addEventListener("click", save)
-document.getElementById("reset-btn").addEventListener("click", deleted)
+// document.getElementById("increment-btn").addEventListener("click", increment)
+// document.getElementById("save-btn").addEventListener("click", save)
+// document.getElementById("reset-btn").addEventListener("click", deleted)
