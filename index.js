376435c05d9529f45d19fbc095676855a4c545db
faheem-dev0 <154550116+faheem-dev0@users.noreply.github.com>
2023-12-27 17:09:01 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,19 +1,49 @@
+// let saveEl = document.getElementById("save-el")
+// let countEl = document.getElementById("count-el")
+// let count = 0
+
+// function increment() {
+//     count += 1
+//     countEl.textContent = count
+// }
+
+// function save() {
+//     let countStr =" "+ count + " - "
+//     saveEl.textContent += countStr
+//     countEl.textContent = 0
+//     count = 0
+// }
+// function deleted() {
+//     countEl.textContent = 0
+//     saveEl.textContent=" " + "Previous entries:" 
+// }
+
+// Get references to elements
 let saveEl = document.getElementById("save-el")
 let countEl = document.getElementById("count-el")
 let count = 0
 
+// Function to handle incrementing count
 function increment() {
     count += 1
     countEl.textContent = count
 }
 
+// Function to handle saving count
 function save() {
     let countStr =" "+ count + " - "
     saveEl.textContent += countStr
     countEl.textContent = 0
     count = 0
 }
+
+// Function to handle resetting count
 function deleted() {
     countEl.textContent = 0
-    saveEl.textContent=" " + "Previous entries:" 
+    saveEl.textContent = " " + "Previous entries:" 
 }
+
+// Event listeners for buttons
+document.getElementById("increment-btn").addEventListener("click", increment)
+document.getElementById("save-btn").addEventListener("click", save)
+document.getElementById("reset-btn").addEventListener("click", deleted)
