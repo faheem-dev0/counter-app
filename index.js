f3519c901c42fafcc1ef86384d9b18813432ee28
faheem-dev0 <154550116+faheem-dev0@users.noreply.github.com>
2023-12-27 19:26:42 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,24 +1,3 @@
-// let saveEl = document.getElementById("save-el")
-// let countEl = document.getElementById("count-el")
-// let count = 0
-
-// function increment() {
-//     count += 1
-//     countEl.textContent = count
-// }
-
-// function save() {
-//     let countStr =" "+ count + " - "
-//     saveEl.textContent += countStr
-//     countEl.textContent = 0
-//     count = 0
-// }
-// function deleted() {
-//     countEl.textContent = 0
-//     saveEl.textContent=" " + "Previous entries:" 
-// }
-
-// Get references to elements
 let saveEl = document.getElementById("save-el")
 let countEl = document.getElementById("count-el")
 let count = 0
